e9006a3f8cce6e99d993e342cde436f12cc68495
Shubham <shubhamkashyap107@gmail.com>
2026-09-18 16:34:16 +0530

    Add Owner's APIs for admin, add admin controllers

--- src/Controllers/owner.controller.js
+++ src/Controllers/owner.controller.js
@@ -1,6 +1,9 @@
 const { AppError } = require("../Utils/AppError")
 const { Organization } = require("../Models/Organization.schema")
 const mongoose = require("mongoose")
+const { User } = require("../Models/User.schema")
+const validator = require("validator")
+const bcrypt = require("bcrypt")
 
 
 const createOrg = async(req, res) => {
@@ -125,9 +128,194 @@ const updateOrg =  async(req, res) => {
 
 }
 
+const createAdmin = async(req, res) => {
+
+    const{ id } = req.params
+
+    if(!id || !mongoose.Types.ObjectId.isValid(id))
+    {
+        throw new AppError(400,"Invalid ID")
+    }
+
+    const{ email, password, name} = req.body
+
+    if(!validator.isEmail(email))
+    {
+        throw new AppError(400, `${email} is not a valid email`)
+    }
+
+    if(!validator.isStrongPassword(password))
+    {
+        throw new AppError(400, `${password} is not a strong password`)
+    }
+
+    if(!name.trim() || name.trim().length > 20 || name.trim().length < 2)
+    {
+        throw new AppError(400, "Invalid name")
+    }
+
+    const foundOrg = await Organization.findById(id)
+
+    if(!foundOrg)
+    {
+        throw new AppError(404, "Organization does not exists")
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const createdAdmin = await User.create({
+        name,
+        password : hashedPassword,
+        email,
+        role : "admin",
+        organizationId : id,
+        isActive : foundOrg.isActive
+    })
+
+    // if(!foundOrg.isActive)
+    // {
+    //     throw new AppError(400, "Organization Inactive")
+    // }
+    
+
+    res
+    .status(201)
+    .json({
+        data : createdAdmin,
+        message : foundOrg.isActive ?  
+        `Admin created under org ${foundOrg.name}` : 
+        `Admin created under org ${foundOrg.name} which is currently INACTIVE`
+    })
+
+}
+
+const getAllAdmins = async(req, res) => {
+
+    const{ id } = req.params
+
+    if(!id || !mongoose.Types.ObjectId.isValid(id))
+    {
+        throw new AppError(400,"Invalid ID")
+    }
+
+    const foundOrg = await Organization.findById(id)
+
+    if(!foundOrg)
+    {
+        throw new AppError(404, "Organization does not exists")
+    }
+
+    const foundAdmins = await User.find({
+        organizationId : foundOrg._id,
+        role : "admin"
+    })
+
+
+    res
+    .status(200)
+    .json({
+        data : foundAdmins,
+        message : foundOrg.isActive ?  
+        `Organization ACTIVE` : 
+        `Organization INACTIVE`
+    })
+
+
+}
+
+
+const getAdminById = async(req, res) => {
+
+    const{ id } = req.params
+
+    if(!id || !mongoose.Types.ObjectId.isValid(id))
+    {
+        throw new AppError(400,"Invalid ID")
+    }
+
+    const foundUser = await User.findById(id)
+
+    if(!foundUser)
+    {
+        throw new AppError(404, "User does not exists")
+    }
+
+    const foundOrg = await Organization.findById(foundUser.organizationId)
+
+    res
+    .status(200)
+    .json({
+        data : foundUser,
+        message : foundOrg.isActive ?  
+        `Organization ACTIVE` : 
+        `Organization INACTIVE`
+    })
+
+
+
+
+}
+
+
+const activateAdmin = async(req, res) => {
+    const{ id } = req.params
+
+    if(!id || !mongoose.Types.ObjectId.isValid(id))
+    {
+        throw new AppError(400,"Invalid ID")
+    }
+
+    const foundUser = await User.findById(id)
+
+    if(!foundUser)
+    {
+        throw new AppError(404, "User does not exists")
+    }
+
+    foundUser.isActive = true
+
+
+    await foundUser.save()
+
+    res
+    .status(200)
+    .json({
+        message : `${foundUser.name} activated successfully`,
+        data : foundUser
+    })
+}
+
+const deactivateAdmin = async(req, res) => {
+      const{ id } = req.params
+
+    if(!id || !mongoose.Types.ObjectId.isValid(id))
+    {
+        throw new AppError(400,"Invalid ID")
+    }
+
+    const foundUser = await User.findById(id)
+
+    if(!foundUser)
+    {
+        throw new AppError(404, "User does not exists")
+    }
+
+    foundUser.isActive = false
+
+
+    await foundUser.save()
+
+    res
+    .status(200)
+    .json({
+        message : `${foundUser.name} deactivated successfully`,
+        data : foundUser
+    })
+}
 
 
 
 module.exports = {
-    createOrg, getAllOrgs, getOrgsById, deleteOrg, updateOrg
+    deactivateAdmin ,createOrg, getAllOrgs, getOrgsById, deleteOrg, updateOrg, createAdmin, getAllAdmins, getAdminById, activateAdmin
 }
--- src/Routes/owner.routes.js
+++ src/Routes/owner.routes.js
@@ -1,18 +1,53 @@
 const express = require("express")
 const { isLoggedIn } = require("../Middlewares/isLoggedIn")
 const { authorize } = require("../Middlewares/authorize")
-const{ createOrg, getAllOrgs, getOrgsById, deleteOrg, updateOrg} = require('../Controllers/owner.controller')
 
+const
+{ 
+    createOrg, 
+    getAllOrgs, 
+    getOrgsById, 
+    deleteOrg, 
+    updateOrg, 
+    createAdmin, 
+    getAllAdmins, 
+    getAdminById,
+    activateAdmin,
+    deactivateAdmin
+    
+} = require('../Controllers/owner.controller')
 const router = express.Router()
 
 
-router.post("/", isLoggedIn, authorize('owner'), createOrg)
+
+/*
+*
+*
+    Owner APIs for Orgs
+* 
+* 
+*/
+
+router.post("/", isLoggedIn, authorize("owner"), createOrg)
 router.get("/", isLoggedIn, authorize("owner"), getAllOrgs)
 router.get("/:id", isLoggedIn, authorize("owner"), getOrgsById)
 router.delete("/:id", isLoggedIn, authorize("owner"), deleteOrg)
 router.patch("/:id", isLoggedIn, authorize("owner"), updateOrg)
 
 
+/*
+  - Owner APIs for Admin
+  - Owner APIs for Admin
+  - Owner APIs for Admin
+  - Owner APIs for Admin
+  - Owner APIs for Admin
+*/
+
+router.post("/organization/:id/admin", isLoggedIn, authorize("owner"), createAdmin)
+router.get("/organization/:id/admin", isLoggedIn, authorize("owner"), getAllAdmins)
+router.get("/admin/:id", isLoggedIn, authorize("owner"), getAdminById)
+router.patch("/admin/:id", isLoggedIn, authorize("owner"), activateAdmin)
+router.delete("/admin/:id", isLoggedIn, authorize("owner"), deactivateAdmin)
 
 
 module.exports = {
